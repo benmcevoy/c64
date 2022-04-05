cfb5ca96a587ab97533caefcd5cf6876145eeb00
ben <ben.mcevoy@gmail.com>
2022-04-05 17:28:26 +1000

    optimise a little, there is no y component when rotating

--- Parametric/parametric.asm
+++ Parametric/parametric.asm
@@ -41,7 +41,6 @@ BasicUpstart2(Start)
 
 #import "_prelude.lib"
 #import "_charscreen.lib"
-#import "_joystick.lib"
 #import "_math.lib"
 
 .label ClearScreen = $E544
@@ -69,12 +68,14 @@ Start: {
 }
 
 Update: {
-    // set point, just moving along a line
-    .var y = CENTERY
-    .var x = time
-
     Set i:#0
     inc startAngle
+    // xRelative is signed and relative to the origin at (CENTERX, CENTERY)
+    lda time
+    sec
+    sbc #CENTERX
+    sta xRelative+1
+    Set xRelative:#0
 
     axis:
         inc writePointer
@@ -94,7 +95,7 @@ Update: {
         Set CharScreen.PenColor:#BLACK
         Call CharScreen.PlotH:x1:y1
 
-        Rotate startAngle:x:#y
+        Rotate startAngle
         Modulo __val0:#WIDTH
         Set x1:__val0
         Modulo __val1:#HEIGHT
@@ -158,20 +159,7 @@ Update: {
     erasePointer: .byte 0
 }
 
-.pseudocommand Rotate angle:x:y{
-    // xRelative is signed and relative to the origin at (CENTERX, CENTERY)
-    lda x
-    sec
-    sbc #CENTERX
-    sta xRelative+1
-    Set xRelative:#0
-
-    lda y
-    sec
-    sbc #CENTERY
-    sta yRelative+1
-    Set yRelative:#0
-
+.pseudocommand Rotate angle {
     ldx angle
     lda cosine,X
     sta cosineAngle
@@ -182,63 +170,27 @@ Update: {
     sta sineAngle
     Sat16 sineAngle:sineAngle+1
 
-    // var x2 = xRel * Math.Cos(angle) - yRel * Math.Sin(angle);
+    // var x2 = xRel * Math.Cos(angle)  ///  yRel is always 0 so ignore this - yRel * Math.Sin(angle);
     Set __tmp0:xRelative
     Set __tmp1:xRelative+1
     Set __tmp2:cosineAngle
     Set __tmp3:cosineAngle+1
     
     SMulW32 __tmp0:__tmp1:__tmp2:__tmp3
-    Set x2:__val1
-    Set x2+1:__val2
-
-    Set __tmp0:yRelative
-    Set __tmp1:yRelative+1
-    Set __tmp2:sineAngle
-    Set __tmp3:sineAngle+1
-
-    SMulW32 __tmp0:__tmp1:__tmp2:__tmp3
-
-    Set y2:__val1
-    Set y2+1:__val2
-
-    Set __tmp0:x2
-    Set __tmp1:x2+1
-    Set __tmp2:y2
-    Set __tmp3:y2+1
     
-    Sub16 __tmp0:__tmp1:__tmp2:__tmp3
     // only care about high byte
-    lda __val1
+    lda __val2
     asl
     sta x1
 
-    // var y2 = x * Math.Sin(angle) + y * Math.Cos(angle);
+    // var y2 = x * Math.Sin(angle) /// -- ignore Y term -- + y * Math.Cos(angle);
     Set __tmp0:xRelative
     Set __tmp1:xRelative+1
     Set __tmp2:sineAngle
     Set __tmp3:sineAngle+1
 
     SMulW32 __tmp0:__tmp1:__tmp2:__tmp3
-    Set x2:__val1
-    Set x2+1:__val2
-
-    Set __tmp0:yRelative
-    Set __tmp1:yRelative+1
-    Set __tmp2:cosineAngle
-    Set __tmp3:cosineAngle+1
-
-    SMulW32 __tmp0:__tmp1:__tmp2:__tmp3
-    Set y2:__val1
-    Set y2+1:__val2
-
-    Set __tmp0:x2
-    Set __tmp1:x2+1
-    Set __tmp2:y2
-    Set __tmp3:y2+1
-
-    Add16 __tmp0:__tmp1:__tmp2:__tmp3
-    lda __val1
+    lda __val2
     asl
     sta y1
 
@@ -257,13 +209,8 @@ Update: {
 // relative to origin at centerx,y
 xRelative: .word 0
 yRelative: .word 0
-
 sineAngle: .word 0
 cosineAngle: .word 0
-
-x2: .word 0
-y2: .word 0
-
 x1: .byte 0
 y1: .byte 0
 
@@ -271,11 +218,11 @@ y1: .byte 0
 time: .byte 0
 palette: .byte 6,11,4,14,5,3,13,7,1,1,7,13,15,5,12,8,2,9,2,9
 
-*=$4000 "Signed trig tables"
+*=$3600 "Signed trig tables"
 // values range -127..127  
 cosine: .fill 256,round(127*cos(toRadians(i*360/256)))
 sine: .fill 256,round(127*sin(toRadians(i*360/256)))
-* = $4200 "trails"
+* = $3800 "trails"
 xTrails: .fill (TRAILS*AXIS),0
 yTrails: .fill (TRAILS*AXIS),0
 
