2717cc3c84915cf3c3fd6adb9d1e3eeadba26596
ben <ben.mcevoy@gmail.com>
2022-06-10 11:16:47 +1000

    experimenting with classic synth ideas

--- Spikes/SIdVisual/controlsignal.asm
+++ Spikes/SIdVisual/controlsignal.asm
@@ -10,9 +10,9 @@ BasicUpstart2(Start)
     .pseudopc SID { .fill 30,0 }
 
     .const VOICE1 = 0
-    .const VOICE2 = 1
-    .const VOICE3 = 2
-    .const MIX = 3
+    .const VOICE2 = 1*7
+    .const VOICE3 = 2*7
+    .const MIX = 3*7
 
     // voice
     .const FREQ_LO = 0
@@ -51,39 +51,80 @@ Start: {
     Set SID+SUSTAIN_RELEASE:#$A2
 
     // v3 plays a low freq
-    Set SID+VOICE3*7+FREQ_LO:#10
-    Set SID+VOICE3*7+FREQ_HI:#0
-    Set SID+VOICE3*7+ATTACK_DECAY:#11
-    Set SID+VOICE3*7+SUSTAIN_RELEASE:#$00
+    Set SID+VOICE3+FREQ_LO:#10
+    Set SID+VOICE3+FREQ_HI:#0
+    Set SID+VOICE3+ATTACK_DECAY:#11
+    Set SID+VOICE3+SUSTAIN_RELEASE:#$00
 
-    Set SID+VOICE3*7+PW_LO:#0
-    Set SID+VOICE3*7+PW_HI:#0
+    Set SID+VOICE3+PW_LO:#0
+    Set SID+VOICE3+PW_HI:#0
 
     // saw, gate on
     Set SID+CONTROL:#%00100001
-    Set SID+VOICE3*7+CONTROL:#%00010000
+    Set SID+VOICE3+CONTROL:#%00010000
 
 
-    Set SID+MIX*7+FILTER_CUT_OFF_LO:#%00000111
-    Set SID+MIX*7+FILTER_CUT_OFF_HI:#%00001111
+    Set SID+MIX+FILTER_CUT_OFF_LO:#%00000111
+    Set SID+MIX+FILTER_CUT_OFF_HI:#%00001111
     // try and get some interest with the filter
-    Set SID+MIX*7+FILTER_CONTROL:#%11110100
+    Set SID+MIX+FILTER_CONTROL:#%11110100
     // v3 off, max volume, lpf on
-    Set SID+MIX*7+VOLUME:#%10011111
+    Set SID+MIX+VOLUME:#%10011111
 
-    PWM()
+    VCO()
 
     rts
 }
 
-.macro Ring(){
-    // TODO: not sure what this will sound like, but basically it's like AM amplitude mod
+// things tried and abandoned
+// VCA - you cannot manipulate the sustain level without retriggering and it sounds crap
+// Ring - nothing special, it's just ring mod.
+// Accent - see VCA.  Instead, set an ADSR, then for each note set a sustain level
+//  it will at least sustain at different levels, and maybe rest the sustain to the
+//  the instrument default afterwards
+// test bit - strobe test bit continously with noise, yeah nah
+
+.macro Jiffy(){
+    // investigate the JIFFY clock
+    
+    
+}
+
+.macro Ratchet(){
+    // and ratcheting
+    // https://github.com/RohanM/clock-with-shift/blob/master/clockwithshift.ino
+}
+
+.macro Reverb(){
+    // is it possible? ping pong to 2 two voices, retrigger with just attack and release
+    // each attack retrigger is quieter
+}
+
+.macro Delay() {
+    // try a circular buffer
+    // or just clock slew?
+}
+
+.macro VCR(){
+    // resonance
+    // v3 freq
+    Set SID+VOICE3+FREQ_LO:#20
+    Set SID+VOICE3+FREQ_HI:#0
+    
+    loop:
+        // v3 amplitude/waveform output
+        lda $D41B
+        and #%11110000
+        ora #%00000001
+        sta SID+MIX+FILTER_CONTROL
+
+    jmp loop    
 }
 
 .macro PWM(){
     // v3 freq
-    Set SID+VOICE3*7+FREQ_LO:#04
-    Set SID+VOICE3*7+FREQ_HI:#0
+    Set SID+VOICE3+FREQ_LO:#04
+    Set SID+VOICE3+FREQ_HI:#0
     // ensure square wave on v1
     Set SID+CONTROL:#%01000001
 
@@ -98,8 +139,11 @@ Start: {
 
 .macro VCO(){
     // v3 freq
-    Set SID+VOICE3*7+FREQ_LO:#20
-    Set SID+VOICE3*7+FREQ_HI:#0
+    Set SID+VOICE3+FREQ_LO:#20
+    Set SID+VOICE3+FREQ_HI:#0
+
+    // different waveforms, saw, triangle. pulse sounds no good
+    Set SID+VOICE3+CONTROL:#%00100000
     
     loop:
         // v3 amplitude/waveform output
@@ -111,25 +155,25 @@ Start: {
 
 .macro VCF(){
     // v3 freq acts speed
-    Set SID+VOICE3*7+FREQ_LO:#12
+    Set SID+VOICE3+FREQ_LO:#12
     // keep this zero, or it's too fast
-    Set SID+VOICE3*7+FREQ_HI:#0
+    Set SID+VOICE3+FREQ_HI:#0
 
     // some noise, or triangle to go up-down
-    Set SID+VOICE3*7+CONTROL:#%10000000
+    Set SID+VOICE3+CONTROL:#%10000000
 
     // filter controls
-    Set SID+MIX*7+FILTER_CUT_OFF_LO:#%00000111
-    Set SID+MIX*7+FILTER_CUT_OFF_HI:#%00000001
+    Set SID+MIX+FILTER_CUT_OFF_LO:#%00000111
+    Set SID+MIX+FILTER_CUT_OFF_HI:#%00000001
     // ensure filter on V1 so we can hear it
-    Set SID+MIX*7+FILTER_CONTROL:#%01110001
+    Set SID+MIX+FILTER_CONTROL:#%01110001
     // v3 off, max volume, lpf on
-    Set SID+MIX*7+VOLUME:#%10011111
+    Set SID+MIX+VOLUME:#%10011111
     
     loop:
         // v3 amplitude/waveform output
         lda $D41B
-        sta SID+MIX*7+FILTER_CUT_OFF_HI
+        sta SID+MIX+FILTER_CUT_OFF_HI
 
     jmp loop
 }
@@ -138,10 +182,10 @@ Start: {
  
     // v3 envelope
     // the large the values the longer the period
-    Set SID+VOICE3*7+ATTACK_DECAY:#$CF
+    Set SID+VOICE3+ATTACK_DECAY:#$CF
     // as we never trigger the release might as well be zero
     // if you set sustain to some value then decay will stop there
-    Set SID+VOICE3*7+SUSTAIN_RELEASE:#$00
+    Set SID+VOICE3+SUSTAIN_RELEASE:#$00
     
     loop:
         // v3 envelope output
